8c0b6f5877fcfa5b5705e25d5a124602de777dee
pc <tiznadoerick3@gmail.com>
2023-10-21 13:03:53 -0600

    Trabajo cambios

--- js/trabajo.js
+++ js/trabajo.js
@@ -1,75 +1,4 @@
-const predefinedTimeBlocks = [
-    "07:00-08:30",
-    "08:40-10:10",
-    "10:20-11:50",
-    "12:00-13:30",
-    "13:40-15:10",
-    "15:20-16:50",
-    "17:00-18:30",
-    "18:40-20:10",
-    "20:20-21:50"
-];
 
-function fillCellBasedOnDay(row, dia, nombre, sala, profesor, color, id, hora_inicio, hora_fin) {
-    let cellContent = `Materia: ${nombre}<br>Docente: ${profesor} (Aula: ${sala})`;
-    
-    let diaIndex;
-    switch(dia) {
-        case 'Lunes': diaIndex = 1; break;
-        case 'Martes': diaIndex = 2; break;
-        case 'Miércoles': diaIndex = 3; break;
-        case 'Jueves': diaIndex = 4; break;
-        case 'Viernes': diaIndex = 5; break;
-        case 'Sabado': diaIndex = 6; break;
-        case 'Domingo': diaIndex = 7; break;
-    }
-
-    const cell = row.cells[diaIndex];
-    cell.innerHTML = cellContent;
-    cell.style.backgroundColor = color;
-}
-
-function loadSchedule() {
-    const userId = localStorage.getItem('userId');
-    
-    // Limpia el contenido existente de la tabla
-    const workScheduleTableBody = document.getElementById('workScheduleTable').getElementsByTagName('tbody')[0];
-    workScheduleTableBody.innerHTML = ''; 
-
-    // Preparar la tabla con los horarios predefinidos
-    predefinedTimeBlocks.forEach(timeBlock => {
-        const row = workScheduleTableBody.insertRow();
-        const cellHorario = row.insertCell(0);
-        cellHorario.textContent = timeBlock;
-        for (let i = 0; i < 7; i++) {
-            row.insertCell(i + 1);
-        }
-    });
-
-    // Solicita las clases
-    fetch(`http://localhost:3000/getClasses?usuario_id=${userId}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(classes => {
-            console.log('Classes loaded from server:', classes);
-
-            classes.forEach(clase => {
-                const timeBlock = `${clase.hora_inicio} - ${clase.hora_fin}`;
-                const rowIndex = predefinedTimeBlocks.indexOf(timeBlock);
-                if (rowIndex !== -1) {
-                    const row = workScheduleTableBody.rows[rowIndex];
-                    fillCellBasedOnDay(row, clase.dias, clase.nombre, clase.sala, clase.profesor, clase.color, clase.ID, clase.hora_inicio, clase.hora_fin);
-                }
-            });
-        })
-        .catch(error => {
-            console.error('Hubo un problema con la petición Fetch:', error);
-        });
-}
 
 
 document.getElementById('addWorkBlockForm').addEventListener('submit', function(event) {
@@ -111,4 +40,4 @@ document.getElementById('addWorkBlockForm').addEventListener('submit', function(
     });
 });
 
-window.onload = loadSchedule;
+
